908f58880de638f3fd75c01158a4ee0356bd54fa
asdf <asdf@gmail.com>
2018-12-13 19:31:52 +0530

    Add thunk npm

--- src/app/actions/userActions.js
+++ src/app/actions/userActions.js
@@ -1,8 +1,15 @@
+import { setTimeout } from "timers";
+
 export function setName(name) {
-    return {
-        type: "SET_NAME",
-        payload: name
+    return dispatch => {
+        setTimeout(() => {
+            dispatch({
+                type: "SET_NAME",
+                payload: name
+            })
+        }, 2000);
     }
+
 }
 
 export function setAge(age) {
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 
 import logger from "redux-logger";
+import thunk from "redux-thunk";
+
 import mathReducer from "./reducers/mathReducer";
 import userReducer from "./reducers/userReducer";
 export default createStore(combineReducers(
     { math: mathReducer, user: userReducer }),
     {},
-    applyMiddleware(logger)
+    applyMiddleware(logger, thunk)
 );
